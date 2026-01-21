5569fb001ae0b0c85f17aed0e40415fe554cc155
jlarm <117611704+jlarm@users.noreply.github.com>
2026-01-21 10:10:42 -0600

    Unguard models and updated user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,29 +4,27 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
+use Carbon\CarbonInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
+/**
+ * @property-read int $id
+ * @property-read string $name
+ * @property-read string $email
+ * @property-read string|null $email_verified_at
+ * @property-read string $password
+ * @property-read CarbonInterface $two_factor_confirmed_at
+ * @property-read  CarbonInterface $created_at
+ * @property-read  CarbonInterface $updated_at
+ */
 final class User extends Authenticatable
 {
     use HasFactory;
     use Notifiable;
     use TwoFactorAuthenticatable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -48,9 +46,14 @@ final class User extends Authenticatable
     protected function casts(): array
     {
         return [
+            'id' => 'integer',
+            'name' => 'string',
+            'email' => 'string',
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
         ];
     }
 }
